065d15ec49dfe01869d734b1f0af9e44c18ca8ac
Roman Kovac <musosoft@gmail.com>
2024-11-04 04:53:32 +0700

    Fix env

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import cloudflare from '@astrojs/cloudflare';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: process.env.PUBLIC_SITE_URL || 'http://localhost:4321',
+  site: import.meta.env.PUBLIC_SITE_URL ?? process.env.PUBLIC_SITE_URL || 'http://localhost:4321',
   output: 'server', // Enables server-side rendering
   adapter: cloudflare({ mode: 'directory' }),
   integrations: [tailwind()],
--- src/turso.ts
+++ src/turso.ts
@@ -2,6 +2,6 @@
 import { createClient } from '@libsql/client/web';
 
 export const turso = createClient({
-  url: import.meta.env.TURSO_DATABASE_URL,
-  authToken: import.meta.env.TURSO_AUTH_TOKEN,
+  url: import.meta.env.TURSO_DATABASE_URL ?? process.env.TURSO_DATABASE_URL,
+  authToken: import.meta.env.TURSO_AUTH_TOKEN ?? process.env.TURSO_AUTH_TOKEN,
 });
