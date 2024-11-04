7f1b99a26d0f989a074a7620876d3fb053aaeb33
Roman Kovac <musosoft@gmail.com>
2024-11-05 05:46:57 +0700

    Try PUBLIC_SITE_URL env

--- src/pages/api/shoutbox.ts
+++ src/pages/api/shoutbox.ts
@@ -2,7 +2,7 @@
 import { turso } from '../../turso';
 
 console.log('API Route Environment Variables:', {
-  TURSO_DATABASE_URL: import.meta.env.TURSO_DATABASE_URL || process.env.TURSO_DATABASE_URL,
+  TURSO_DATAPUBLIC_SITE_URL: import.meta.env.TURSO_DATAPUBLIC_SITE_URL || process.env.TURSO_DATAPUBLIC_SITE_URL,
   TURSO_AUTH_TOKEN: import.meta.env.TURSO_AUTH_TOKEN || process.env.TURSO_AUTH_TOKEN,
 });
 
