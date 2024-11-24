db6f1165401bf9c25021d8e705cd38c17f4570ef
Roman Kovac <musosoft@gmail.com>
2024-11-25 02:31:58 +0700

    Fluid design

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,4 +1,4 @@
-// .prettierrc.mjs
+// prettier.config.mjs
 /** @type {import("prettier").Config} */
 export default {
   plugins: ["prettier-plugin-astro"],
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,11 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 // tailwind.config.mjs
+import fluid, { extract, screens, fontSize } from "fluid-tailwind";
+
 export default {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,ts,tsx,vue}"],
+  content: {
+    files: ["./src/**/*.{astro,html,js,jsx,md,mdx,ts,tsx,vue}"],
+    extract,
+  },
   theme: {
+    screens, // Tailwind's default screens, in `rem`
+    fontSize, // Tailwind's default font sizes, in `rem` (including line heights)
     extend: {},
   },
   plugins: [
+    fluid,
     function ({ addComponents }) {
       addComponents({
         ".btn": {
